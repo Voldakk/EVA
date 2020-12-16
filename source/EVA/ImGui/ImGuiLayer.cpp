65a66f2132f0f0822ea8d8ba03e5159083302271
Voldakk <voldakk@outlook.com>
2020-12-16 12:17:04 +0100

    Updated dependencies

--- source/EVA/ImGui/ImGuiLayer.cpp
+++ source/EVA/ImGui/ImGuiLayer.cpp
@@ -3,8 +3,8 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 #include <imgui.h>
-#include <examples/imgui_impl_glfw.h>
-#include <examples/imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
 #include "EVA/Core/Application.hpp"
 
